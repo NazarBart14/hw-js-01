67da4807ab7f0bdd9e661b14dcd2eb76082be584
Bartoshuk Nazar Dmytrovych <bartoshuknazar0602@gmail.com>
2023-12-12 11:53:29 +0200

    DO_DZ

--- DZ_JS/Task-5.js
+++ DZ_JS/Task-5.js
@@ -1,1 +1,25 @@
-// Вибачте в мене це не вийшло
+let input;
+const numbers = [];
+let total = 0;
+
+do {
+  input = prompt('Enter a number:');
+
+  if (input !== null) {
+    const number = Number(input);
+
+    if (!isNaN(number)) {
+      numbers.push(number);
+    } else {
+      alert('Invalid input. Please try again.');
+    }
+  }
+} while (input !== null);
+
+if (numbers.length > 0) {
+  for (const number of numbers) {
+    total += number;
+  }
+
+  console.log(`The total sum of numbers is ${total}`);
+}
